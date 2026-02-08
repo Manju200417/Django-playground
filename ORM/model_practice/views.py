d4f76222b0819e59d1a5935eb523743ca481c879
Manju200417 <https://github.com/Manju200417>
2026-02-08 10:50:08 +0530

    user orm

--- ORM/model_practice/views.py
+++ ORM/model_practice/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Customer
 
 # Create your views here.
+
+def add_cust(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        customer = Customer.objects.create(
+            name = name,
+            email = email,
+            password = password
+        )
+    return render(request,'add_cust.html') 
