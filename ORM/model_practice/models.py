ee2121c31a557e28d08b046fda3c51fd5bc05e7b
Manju200417 <https://github.com/Manju200417>
2026-02-08 12:48:44 +0530

    added product table using orm

--- ORM/model_practice/models.py
+++ ORM/model_practice/models.py
@@ -6,3 +6,9 @@ class Customer(models.Model):
     name = models.CharField(max_length = 100)
     email = models.EmailField(unique = True)
     password = models.CharField()
+
+
+class Prodect(models.Model):
+    customer = models.ForeignKey(Customer,on_delete = models.CASCADE)
+    name = models.CharField(max_length = 100)
+    price = models.DecimalField(max_digits = 10,decimal_places = 2)
