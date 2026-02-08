f11e1a77b06bf78e5dd70c43c8ca21d8ac4a52cf
Manju200417 <https://github.com/Manju200417>
2026-02-08 18:29:31 +0530

    Admin panel customisation

--- ORM/model_practice/admin.py
+++ ORM/model_practice/admin.py
@@ -4,6 +4,13 @@ from .models import Item,Prodect,Customer
 # Register your models here.
 
 
-admin.site.register(Item)
+# admin.site.register(Item)
 admin.site.register(Prodect)
 admin.site.register(Customer)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
+    list_filter = ('price',)
+    ordering = ('id',)
--- ORM/model_practice/models.py
+++ ORM/model_practice/models.py
@@ -7,13 +7,22 @@ class Customer(models.Model):
     email = models.EmailField(unique = True)
     password = models.CharField()
 
+    def __str__(self):
+        return self.name
+
 
 class Prodect(models.Model):
     customer = models.ForeignKey(Customer,on_delete = models.CASCADE)
     name = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits = 10,decimal_places = 2)
 
+    def __str__(self):
+        return self.name
+
 
 class Item(models.Model):
     name = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits = 10,decimal_places = 2)
+
+    def __str__(self):
+        return self.name
