14fc593b8f1774010801a7f7d01c88972c3f58c7
Manju200417 <https://github.com/Manju200417>
2026-02-08 18:19:37 +0530

    Register models for admin

--- ORM/model_practice/admin.py
+++ ORM/model_practice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item,Prodect,Customer
 
 # Register your models here.
+
+
+admin.site.register(Item)
+admin.site.register(Prodect)
+admin.site.register(Customer)
